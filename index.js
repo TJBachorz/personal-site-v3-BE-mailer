d23f2dfccac708d735ddaf3969a80e952ce92a19
TJBachorz <TJBachorz@gmail.com>
2020-12-23 16:26:38 -0700

    receives email requests from front end

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const app = require("express")();
-const cors = require("cors")
 const sgMail = require("@sendgrid/mail");
+const cors = require("cors")
+const bodyParser = require("body-parser");
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const message = {
-    to: "tjbachorz@gmail.com",
-    from: "tjbachorz@gmail.com",
-    subject: "Message from tjbachorz.live",
-    text: "test text email!",
-    
-};
+app.use(cors())
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+app.use(bodyParser.json());
 
-sgMail
-    .send(message)
-    .then(() => {
-        console.log('Email sent')
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+// sgMail
+//     .send(message)
+//     .then(() => {
+//         console.log('Email sent')
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
+// /mail/send
 
-app.post("/mail/send", (request, response) => {
-    console.log(response)
+app.post("/", (request, response) => {
+    console.log(request.body)
+    response.json("Success")
+    // const email = {
+    //     to: "tjbachorz@gmail.com",
+    //     from: request.body.from,
+    //     subject: `${request.body.full_name} -- ${request.body.subject}`,
+    //     text: request.body.message
+    // }
+    // sgMail.send(email)
+    //     .then(response.json("success"))
+    //     .catch((error) => {
+    //         response.json(error)
+    //     })
 })
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT)
